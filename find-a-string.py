fedf48feecdec3c43d4c1c87d94b609046bfdb6c
parthiv21 <parthiv3003@gmail.com>
2026-01-29 08:33:28 +0530

    Find count of substring in string

---
+++ find-a-string.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/find-a-string
+
+def count_substring(string, sub_string):
+    count = 0
+    string_length = len(string)
+    sub_string_length = len(sub_string)
+
+    if (string_length < sub_string_length):
+        return count
+
+    for i in range(string_length - sub_string_length + 1):
+        if string[i:i + sub_string_length] == sub_string:
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
