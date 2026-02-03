442e592d92e1f9046f7033c087ce87fd7c3d6a0e
parthiv21 <parthiv3003@gmail.com>
2026-02-03 08:27:12 +0530

    designer-door-mat

---
+++ designer-door-mat.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/designer-door-mat/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+
+    for i in range(N // 2):
+        pattern = ".|." * (2 * i + 1)
+        print(pattern.center(M, "-"))
+
+    print("WELCOME".center(M, "-"))
+
+    for i in range(N // 2 - 1, -1, -1):
+        pattern = ".|." * (2 * i + 1)
+        print(pattern.center(M, "-"))
