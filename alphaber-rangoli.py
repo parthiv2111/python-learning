47b42171ecce957be476cc76fb08b2b6ee4f0024
parthiv21 <parthiv3003@gmail.com>
2026-02-05 08:05:54 +0530

    Alphabet Rangoli

---
+++ alphaber-rangoli.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/alphabet-rangoli
+
+import string
+
+
+def print_rangoli(size):
+    alpha = string.ascii_lowercase
+    width = 4 * size - 3
+
+    lines = []
+
+    for i in range(size):
+        left = alpha[size-1:i:-1]     # descending
+        right = alpha[i:size]         # ascending
+        row = "-".join(left + right)
+        lines.append(row.center(width, "-"))
+
+    print("\n".join(lines[::-1] + lines[1:]))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
